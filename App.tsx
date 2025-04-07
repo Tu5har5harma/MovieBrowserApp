134d0ebfe7153ae16fea303011153212042d97be
Tushar <tushar.sharma@in.patrika.com>
2025-04-07 23:21:52 +0530

    Added static splash screen with GIF

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import AppNavigator from "./src/navigation/AppNavigator";
+import SplashScreen from "./src/components/SplashScreen/SplashScreen";
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setAppIsReady(true), 2000);
+  }, []);
+
   return (
     <SafeAreaProvider>
-      <AppNavigator />
+      {appIsReady ? <AppNavigator /> : <SplashScreen />}
     </SafeAreaProvider>
   );
 }
---
+++ src/components/SplashScreen/SplashScreen.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Image, StyleSheet } from "react-native";
+
+export default () => (
+  <View style={styles.container}>
+    <Image
+      source={require("../../../assets/splash.gif")}
+      style={styles.image}
+    />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  image: { width: "100%", height: "100%", resizeMode: "contain" },
+});
